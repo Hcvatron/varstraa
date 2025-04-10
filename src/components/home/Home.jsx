4c821e24935aeae041c89f7dc87f29be2a258c93
Hcvatron <hcvatronwork1001@gmail.com>
2025-04-10 23:37:08 +0530

    'update'

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import Hero from '../hero/Hero'
 import './Home.css'
 import DealSection from '../DealSection/DealSection'
+import Trending from '../Trending/Trending'
+import TopSellers from '../TopSeller/TopSeller'
 
 const Home = () => {
   return (
     <div className='home'>
         <Hero />
         <DealSection />
+        <Trending />
+        <TopSellers />
     </div>
   )
 }
